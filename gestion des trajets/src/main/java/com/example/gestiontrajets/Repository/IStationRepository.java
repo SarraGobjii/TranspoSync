a193b3ec72e61008dcda6bf566e11e7b9bf017f1
Sarra-Bltf <beltayefsarra@gmail.com>
2024-12-27 20:07:48 +0100

    Delete gestion des trajets directory

--- gestion des trajets/src/main/java/com/example/gestiontrajets/Repository/IStationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gestiontrajets.Repository;
-
-import com.example.gestiontrajets.Entities.Station;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IStationRepository extends JpaRepository<Station, Long> {
-}
