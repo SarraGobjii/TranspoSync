ef0a575feb6040aceb216fe6082db592db854ca0
SarraGobjii <sarra.gobji@sesame.com.tn>
2024-12-27 17:55:17 +0100

    new module (spring+maven)

---
+++ TranspoSync1/src/main/java/com/example/modulemavenspring/ModuleMavenSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.modulemavenspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleMavenSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleMavenSpringApplication.class, args);
+    }
+
+}
---
+++ TranspoSync1/src/test/java/com/example/modulemavenspring/ModuleMavenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modulemavenspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleMavenSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
