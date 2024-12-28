3a76088e730cc1d702899bc96fbc066af5779228
Sarra-Bltf <beltayefsarra@gmail.com>
2024-12-29 00:10:33 +0100

    j'ai ajouté les associations,les Controllers de chaque entités,les repository et les services

--- TranspoSync1/src/main/java/com/example/modulemavenspring/repository/IStationRepository.java
+++ TranspoSync1/src/main/java/com/example/modulemavenspring/repository/IStationRepository.java
@@ -1,2 +1,9 @@
-package com.example.modulemavenspring.repository;public interface IStationRepository {
+package com.example.modulemavenspring.repository;
+
+import com.example.modulemavenspring.entities.Station;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IStationRepository extends CrudRepository<Station, Long> {
 }
--- TranspoSync1/src/main/java/com/example/modulemavenspring/repository/Istationrepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.modulemavenspring.repository;
-
-import com.example.modulemavenspring.entities.Station;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IStationRepository extends CrudRepository<Station, Long> {
-}
