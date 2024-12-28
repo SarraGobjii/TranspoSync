da9157a619692391dec463458e5bff63562a93df
Sarra-Bltf <beltayefsarra@gmail.com>
2024-12-28 23:19:37 +0100

    Update Itineraire.java
    new commit

---
+++ TranspoSync1/src/main/java/com/example/modulemavenspring/entities/Itineraire.java
@@ -0,0 +1,25 @@
+package com.example.modulemavenspring.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Itineraire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idItineraire;
+    private String route;
+    private LocalDateTime heureDebut;
+    private LocalDateTime heureFin;
+
+
+
+}
