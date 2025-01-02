ce123f9b9a636a5379475df0f836bcd7c71c5ddf
SarraGobjii <sarra.gobji@sesame.com.tn>
2025-01-02 21:09:58 +0100

    added constructors to itineraire class

--- TranspoSync1/src/main/java/com/example/modulemavenspring/entities/Itineraire.java
+++ TranspoSync1/src/main/java/com/example/modulemavenspring/entities/Itineraire.java
@@ -4,7 +4,9 @@ package com.example.modulemavenspring.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
@@ -13,9 +15,11 @@ import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 
-@Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Itineraire implements Serializable {
 
     @Id
