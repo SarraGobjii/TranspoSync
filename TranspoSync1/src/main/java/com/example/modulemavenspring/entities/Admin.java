0be991ac35aff91684b1eb12703238d80f34cd05
SarraGobjii <sarra.gobji@sesame.com.tn>
2025-01-20 20:30:21 +0100

    entité admin

--- TranspoSync1/src/main/java/com/example/modulemavenspring/entities/Admin.java
+++ TranspoSync1/src/main/java/com/example/modulemavenspring/entities/Admin.java
@@ -20,7 +20,7 @@ import java.io.Serializable;
 public class Admin implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idAdmin;
+    private Long idAdmin;
     private String username;
     private Long password;
 
