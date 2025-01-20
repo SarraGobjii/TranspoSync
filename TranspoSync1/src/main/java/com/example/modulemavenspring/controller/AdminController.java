95d5e2064c92fe7f08c986e03254fc9ca47c25d0
SarraGobjii <sarra.gobji@sesame.com.tn>
2025-01-20 20:54:17 +0100

    Controller Admin

--- TranspoSync1/src/main/java/com/example/modulemavenspring/controller/AdminController.java
+++ TranspoSync1/src/main/java/com/example/modulemavenspring/controller/AdminController.java
@@ -1,10 +1,37 @@
 package com.example.modulemavenspring.controller;
 
+import com.example.modulemavenspring.entities.Admin;
+import com.example.modulemavenspring.repository.IAdminRepository;
+import com.example.modulemavenspring.service.IAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/AdminController")
 public class AdminController {
+    @Autowired
+    private IAdminService adminService;
+    @GetMapping("/getAllAdmin")
+    public List<Admin> getAllAdmin() {
+        return adminService.getAllAdmin();
+    }
+    @GetMapping("GetAdmin/{idAdmin}")
+    public Admin getAdminById(@PathVariable Long idAdmin) {
+        return adminService.getAdminById(idAdmin);
+    }
+    @PostMapping("/addAdmin")
+    public Admin createAdmin(@RequestBody Admin admin) {
+        return adminService.createAdmin(admin);
+    }
+    @PutMapping("/updateAdmin/{idAdmin}")
+    public Admin updateAdmin(@PathVariable Long idAdmin, Admin admin) {
+        return adminService.updateAdmin(admin);
+    }
+    @DeleteMapping("/deleteAdmin{idAdmin}")
+    public void deleteAdmin(@PathVariable Long idAdmin) {
+        adminService.deleteAdmin(idAdmin);
+
+    }
 }
